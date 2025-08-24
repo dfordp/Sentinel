7ac9dddfc78747f890218cfc1221a53327727801
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2025-08-24 15:12:49 +0530

    feat: :sparkles: add oauth app github

--- apps/server/src/routers/index.ts
+++ apps/server/src/routers/index.ts
@@ -2,6 +2,7 @@ import { healthCheckProcedure } from "@/procedures/health";
 import { router } from "../lib/trpc";
 
 export const appRouter = router({
-  "/health/check": healthCheckProcedure
+  "/health/check": healthCheckProcedure,
+  "/auth/github/oauth-url": ,
 });
 export type AppRouter = typeof appRouter;
