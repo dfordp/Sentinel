3fc7f251e26a31b0a27bcea312b221fcf5de0a1b
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2025-10-03 12:54:53 +0530

    feat: :sparkles: add github oauth url

---
+++ apps/server/constants/config.ts
@@ -0,0 +1,1 @@
+export const GITHUB_OAUTH_CALLBACK_PATH = "/auth/login/github/callback";
--- apps/server/src/integrations/github-oauth.ts
+++ apps/server/src/integrations/github-oauth.ts
@@ -1,7 +1,35 @@
 import type { IntegrationTool } from "@/types/integration-tool";
+import {
+	GITHUB_OAUTH_CLIENT_ID,
+	GITHUB_OAUTH_CLIENT_SECRET,
+	HOST,
+} from "@/utils/env";
+import { OAuthApp } from "@octokit/oauth-app";
+import { GITHUB_OAUTH_CALLBACK_PATH } from "constants/config";
 
 export class GithubOAuth implements IntegrationTool {
 	name = "Github OAuth";
 	description =
 		"This integration tools is used to control the whole application";
+
+	githubApp: OAuthApp;
+
+	constructor() {
+		this.githubApp = new OAuthApp({
+			clientType: "oauth-app",
+			clientId: GITHUB_OAUTH_CLIENT_ID,
+			clientSecret: GITHUB_OAUTH_CLIENT_SECRET,
+			redirectUrl: `${HOST}${GITHUB_OAUTH_CALLBACK_PATH}`,
+			defaultScopes: ["read:user", "user:email", "read:org"],
+		});
+	}
+
+	async getOAuthUrl() {
+		const response = this.githubApp.getWebFlowAuthorizationUrl({
+			allowSignup: true,
+			scopes: ["read:user", "user:email", "read:org"],
+		});
+
+		return response.url;
+	}
 }
