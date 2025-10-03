201426a64d717992f7d774138bc87061843fdc1e
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2025-10-03 11:12:34 +0530

    feat: :sparkles: add new env

--- apps/server/src/utils/env.ts
+++ apps/server/src/utils/env.ts
@@ -4,3 +4,6 @@ export const GITHUB_OAUTH_CLIENT_ID = process.env
 	.GITHUB_OAUTH_CLIENT_ID as string;
 export const GITHUB_OAUTH_CLIENT_SECRET = process.env
 	.GITHUB_OAUTH_CLIENT_SECRET as string;
+
+export const DATABASE_URL = process.env.DATABASE_URL as string;
+export const DIRECT_DATABASE_URL = process.env.DIRECT_DATABASE_URL as string;
